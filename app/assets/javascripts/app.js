db0f4fd3a564d08b5399d38bf158b073420c6e7b
tdshap <tess.shapiro@yahoo.com>
2014-10-27 01:46:43 -0400

    put all api info on page

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -16,21 +16,138 @@ var StartEndPoints = Backbone.View.extend({
 		this.render()
 	},
 	render: function(){ 
-		StartEnd = this.$el.html(this.template)
-		$("div.container").append(StartEnd)
+		var StartEnd = this.$el.html(this.template);
+		$("div.container").empty();
+		$("div.container").append(StartEnd);
 	},
 	getStartEndPoints: function(){
 		startingPoint = $("input.start").val()
 		endingPoint = $("input.end").val()
-debugger
 		$.ajax({
 			type: "POST",
 			url: "http://127.0.0.1:3000/route", 
 			data: {
 				startingPoint: startingPoint,
 				endingPoint: endingPoint
-			}
-		}).done
+			},
+		}).done(function(results){
+			var location = new LocationView({ location_results: results })
+			// location.render()
+			var weather = new WeatherView({ weather_results: results })
+			weather.render()
+			var walking = new WalkingView({ walking_results: results })
+			walking.render()
+			var transit = new TransitView({ transit_results: results })
+			transit.render()
+			var biking = new BikingView({ biking_results: results })
+			biking.render()
+			var taxi = new TaxiView({ taxi_results: results })
+			taxi.render()
+		})
+	}
+})
+
+var LocationView = Backbone.View.extend({
+	tagName: "div", 
+	attributes: {
+		class: "location col-xs-12 col-s-12 col-md-12 col-lg-12"
+	},
+	template: _.template( $("#location-names").html() ), 
+		initialize: function(results){
+		start_location = results.location_results.location.start_location,
+		end_location = results.location_results.location.end_location,
+		this.render()
+	},
+	render: function(){
+		var locationView = this.$el.html( this.template() )
+		$("div.container").empty()
+		$("div.container").append(locationView)
+	}
+})
+
+
+var WeatherView = Backbone.View.extend({
+	tagName: "div", 
+	attributes: {
+		class: "weather col-xs-12 col-s-12 col-md-12 col-lg-12"
+	},
+	template: _.template($("#weather-view").html() ),
+	initialize: function(results){
+		conditions = results.weather_results.weather.conditions,
+		temp = results.weather_results.weather.temp,
+		icon = results.weather_results.weather.icon
+	},
+	render: function(){
+		var weatherView = this.$el.html( this.template() )
+		$("div.container").append(weatherView)
+	}
+})
+
+var WalkingView = Backbone.View.extend({
+	tagName: "div",
+	attributes: {
+		class: "walking col-xs-12 col-s-12 col-md-12 col-lg-12"
+	},
+	template: _.template($("#walking-view").html() ),
+	initialize: function(results){
+		distance = results.walking_results.walking.distance,
+		time = results.walking_results.walking.time, 
+		calories = results.walking_results.walking.calories
+	},
+	render: function(){
+		var walkingView = this.$el.html( this.template() )
+		$("div.container").append(walkingView)
+	}
+})
+
+var TransitView = Backbone.View.extend({
+	tagName: "div",
+	attributes: {
+		class: "transit col-xs-12 col-s-12 col-md-12 col-lg-12"
+	},
+	template: _.template($("#transit-view").html() ),
+	initialize: function(results){
+		transit_icon = results.transit_results.transit.transit_icon,
+		route_icon = results.transit_results.transit.route_icon,
+		duration = results.transit_results.transit.duration,
+		distance = results.transit_results.transit.distance
+	},
+	render: function(){
+		var transitView = this.$el.html( this.template() )
+		$("div.container").append(transitView)
+	}
+})
+
+
+var BikingView = Backbone.View.extend({
+	tagName: "div",
+	attributes: {
+		class: "biking col-xs-12 col-s-12 col-md-12 col-lg-12"
+	},
+	template: _.template($("#biking-view").html() ),
+	initialize: function(results){
+		distance = results.biking_results.biking.distance,
+		time = results.biking_results.biking.time,
+		calories = results.biking_results.biking.calories
+	},
+	render: function(){
+		var bikingView = this.$el.html( this.template() )
+		$("div.container").append(bikingView)
+	}
+})
+
+var TaxiView = Backbone.View.extend({
+	tagName: "div",
+	attributes: {
+		class: "taxi col-xs-12 col-s-12 col-md-12 col-lg-12"
+	},
+	template: _.template($("#taxi-view").html() ),
+	initialize: function(results){
+		price = results.taxi_results.taxi.price
+	},
+	render: function(){
+		var taxiView = this.$el.html( this.template() )
+		$("div.container").append(taxiView)
 	}
 })
 
@@ -38,13 +155,30 @@ debugger
 var Router = Backbone.Router.extend({
 	routes: {
 		"": "home",
-		// "/transitOptions": ""
+		"walking": "walking",
+		"transit": "transit",
+		"biking": "biking",
+		"taxi": "taxi"
 	}
 })
 var router = new Router;
-	router.on("route:home", function(){
-	var startEndPoints = new StartEndPoints
-	
+
+router.on("route:home", function(){
+var startEndPoints = new StartEndPoints
+})
+
+router.on("route:walking", function(){
+	console.log("walking router")
+})
+
+router.on("route:transit", function(){
+	console.log("walking router")
+})
+router.on("route:biking", function(){
+	console.log("walking router")
+})
+router.on("route:taxi", function(){
+	console.log("walking router")
 })
 
 
--- app/controllers/route_controller.rb
+++ app/controllers/route_controller.rb
@@ -29,9 +29,9 @@ class RouteController < ApplicationController
 
 		# weather API call based on lat /lgn
 		weather = HTTParty.get("http://api.wunderground.com/api/d8beaac28d7f691e/conditions/q/#{end_lat},#{end_lng}.json")
-		conditions = weather["current_observation"]["weather"]  
+		weather_conditions = weather["current_observation"]["weather"]  
 		temp_f = weather["current_observation"]["temp_f"]  
-		icon = weather["current_observation"]["icon_url"] 
+		weather_icon = weather["current_observation"]["icon_url"] 
 
 
 		# Walking distance 
@@ -44,18 +44,71 @@ class RouteController < ApplicationController
 
 		# Biking distance
 		biking = HTTParty.get("https://maps.googleapis.com/maps/api/distancematrix/json?origins=#{start_lat},#{start_lng}&destinations=#{end_lat},#{end_lng}&mode=bicycling&departure_time=#{Time.now.to_i}&units=imperial")
+		if walking["status"] != "OK"
+			render :text => "An error has occured"
+		end
 		biking_distance = biking["rows"][0]["elements"][0]["distance"]["text"]
 		biking_time = biking["rows"][0]["elements"][0]["duration"]["text"]
-binding.pry
-
-		# google maps transit directions call
-
-			# encoding timestamp to seconds since 1970: Time.now.to_i
-		
 
 
+		# Transit directions
+		transit = HTTParty.get("https://maps.googleapis.com/maps/api/directions/json?origin=#{start_lat},#{start_lng}&destination=#{end_lat},#{end_lng}&mode=transit&departure_time=#{Time.now.to_i}&units=imperial&key=AIzaSyAccMdPuDvaAGvXzwJemlq5ZNJjNxEvEec")
+		transit_distance = transit["routes"][0]["legs"][0]["distance"]["text"]
+		transit_duration = transit["routes"][0]["legs"][0]["duration"]["text"]
+		if transit["routes"][0]["legs"][0]["steps"][1]["transit_details"]
+			transit_icon = transit["routes"][0]["legs"][0]["steps"][1]["transit_details"]["line"]["vehicle"]["icon"]  
+			else 
+				transit_icon = ""
+		end 
+		if transit["routes"][0]["legs"][0]["steps"][1]["transit_details"]["line"]["icon"] 
+			transit_route_icon = transit["routes"][0]["legs"][0]["steps"][1]["transit_details"]["line"]["icon"] 
+		else 
+			transit_route_icon = ""
+		end 
 		# uber taxi time and $ call
+		uber = HTTParty.get("https://api.uber.com/v1/estimates/price?server_token=NE7mzs5jHPxZWhdfsTRVmHM0y5hlr2PA_SqTVTp_&start_latitude=#{start_lat}&start_longitude=#{start_lng}&end_latitude=#{end_lat}&end_longitude=#{end_lng}")
+		if uber["code"] != nil
+			render :text => "An error has occured"
+		end 
+		
+		uberX_estimate = uber["prices"][0]["estimate"]  
+
+results = {
+	:location => {
+		:start_location => start_name,
+		:start_lat => start_lat, 
+		:start_lng => start_lng,
+		:end_location => end_name,
+		:end_lat => end_lat,
+		:end_lng => end_lng
+	},
+	:weather => {
+		:icon => weather_icon,
+		:temp => temp_f,
+		:conditions => weather_conditions
+	},
+	:transit => {
+		:transit_icon => transit_icon,
+		:route_icon => transit_route_icon,
+		:duration => transit_duration,
+		:distance => transit_distance
+	},
+	:biking => {
+		:distance => biking_distance,
+		:time => biking_time,
+		:calories => "figure this out!"
+	},
+	:walking => {
+		:distance => walking_distance,
+		:time => walking_time,
+		:calories => "figure this out too!"
+	},
+	:taxi => {
+		:price => uberX_estimate
+	}
+}
 
 
+	render :json => results
 	end 
 end 
