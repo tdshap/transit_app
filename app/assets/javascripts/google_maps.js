215789eab592506a0df6b5d08187a62b1d1accd6
tdshap <tess.shapiro@yahoo.com>
2014-10-29 16:45:15 -0400

    troubleshooting google maps

--- app/assets/javascripts/google_maps.js
+++ app/assets/javascripts/google_maps.js
@@ -1,4 +1,4 @@
 <script type="text/javascript"
-  src="https://maps.googleapis.com/maps/api/js?key=" + GOOGLE_API_KEY >
+  src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAccMdPuDvaAGvXzwJemlq5ZNJjNxEvEec>
 </script>
 window.onload = loadScript;
